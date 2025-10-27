66224a1b9d71a54ee7681b82afa5b3767d8460e6
charanjithreddy <charanjith.angadi@gmail.com>
2025-10-27 08:46:35 +0530

    rectified error in emp login creation routes

--- routes/employee_login_creation_routes.py
+++ routes/employee_login_creation_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 
-employee_login_creation_bp=Blueprint("employee_login_creation_bp",_name_)
+employee_login_creation_bp=Blueprint("employee_login_creation_bp",__name__)
 
 @employee_login_creation_bp.route("/employee_login_creation.html",methods=['POST','GET'])
 def create_employee():
